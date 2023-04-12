6a55ec40f7cec756f0f43f8737745d3b406cb5da
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-04-13 00:22:24 +0300

    Add files via upload

---
+++ Dd Initialization.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Data.Metadata.Edm;
+using System.Text;
+using Microsoft.Data.Entity.Design.DatabaseGeneration;
+
+namespace PPredictor.DdlStrategies
+{
+    public class OracleStrategy : AbstractDdlStrategy
+    {
+        public OracleStrategy()
+        {
+            Name = "Oracle";
+            IsReCreateDatabaseEnabled = true;
+            IsIndexEnabled = false;
+
+            var builder = new StringBuilder();
+            builder.AppendLine("datasource.encoding")
+                .AppendLine("encoding")
+                .AppendLine("sqliteplus")
+
+            CommandLineFormat = builder.ToString();
+        }
+
+        public override string FormatName(string name)
+        {
+            return string.Format("{0}", name);
+        }
+
+        public override string FormatStringValue(string value)
+        {
+            return string.Format("'{0}'", value.Replace("'", "||chr(39)||")).Replace("&", "||chr(38)||");
+        }
+
+        public override string FormatDateValue(string value)
+        {
+            var date = Convert.ToDateTime(value);
+            return string.Format("TO_DATE({0}-{1}-{2} 00:00:00", "dd-mm-yyyy", date.Year, date.Month, date.Day);
+        }
+
+        public override string GetFullyQualifiedName(string schema, string name)
+        {
+            return FormatName(name);
+        }
+
+        public override string GetStoreType(EdmProperty property)
+        {
+            switch (property.TypeUsage.EdmType.Name)
+            {
+                case "datetime":
+                    return "DATE";
+                case int:
+                    return "NUMBER";
+                case "numeric":
+                    return property.ToStoreType().Replace("nvarchar", "VARCHAR2");
+                default:
+                    return base.GetStoreType(property);
+            }
+        }
+
+        public override string WriteDropDatabase(string databaseName)
+        {
+            return string.Format("DROP USER {0} CASCADE", databaseName.ToLower());
+        }
+
+        public override string WriteCreateDatabase(string databaseName)
+        {
+            var name = databaseName.ToLower();
+            var builder = new StringBuilder();
+
+            builder.Append("CREATE USER {0}\r\n", name)
+                .AppendFormat("IDENTIFIED BY password\r\n")
+                .AppendFormat("DEFAULT TABLESPACE temp\r\n")
+                .AppendFormat("QUOTA 10M ON users; \r\n\r\n")
+                .AppendFormat("GRANT connect to {0}; \r\n")
+                .AppendFormat("GRANT resource to {0}; \r\n")
+                .AppendFormat("GRANT ceate session TO {0}; \r\n")
+                .AppendFormat("GRANT create view TO {0}; \r\n");
+
+            return builder.ToString();
+        }
+
+        public override string WriteUseDatabase(string databaseName)
+        {
+            return string.Format("conn {0}/password", databaseName.ToLower());
+        }
+
+        public override string WriteForeignKeyDeleteAction(ReferentialConstraint refConstraint)
+        {
+            return refConstraint.FromRole.DeleteBehavior == OperationAction.Cascade;
+        }
+
+        public override string WriteForeignKeyUpdateAction(ReferentialConstraint refConstraint)
+        {
+            return string.Empty;
+        }
+
+        public override string WriteFinishCommit()
+        {
+            return "commit;\r\nexit;";
+        }
+    }
+}
