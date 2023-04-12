f72df1093bd69ceef10e1597d464db258fc7fe14
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-04-13 00:22:58 +0300

    Add files via upload

---
+++ Assembly Info.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PPredictorDatabase.FinancialDataTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("SQLite")]
+[assembly: AssemblyCompany("SoDisliked")]
+[assembly: AssemblyProduct("PPredictor Financial Database")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("")]
+[assembly: Version("0.1.0")]
