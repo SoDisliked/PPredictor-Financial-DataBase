5e66ffcefc885134a1836f187c4c03b24ea43abe
L Frumuselu <90557319+SoDisliked@users.noreply.github.com>
2023-04-13 00:22:42 +0300

    Add files via upload

---
+++ User info.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Cache;
+using System.Data.Entity.Spatial;
+
+[Table("User")]
+public partial class User
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "Error while getting the ID of the user."]
+    public User()
+    {
+        this.Transaction = new HashSet<Transaction>();
+    }
+
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Password { get; set; }
+
+    [StringLength(1000)]
+    public string FinancialInformation { get; set; }
+
+    public bool FinancialDisplay { get; set; }
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "Error while displaying all of the financial information of the user")]
+    public virtual ICollection<Transaction> Transactions { get; set; }
+}
